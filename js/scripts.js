68eb81d27d27123148e391ca869449a0877d1bff
Jhonathanmdx <jhonathan.mdx@gmail.com>
2021-02-25 23:16:35 -0300

    upgrade

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-window.addEventListener('load', (event) => {
-    console.log('The page has fully loaded');
-});
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        modalOverlay.classList.add('active')
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+})
